ee3c3e025f3d511f7f85509ff7abbf1534b1ca91
lurcheous73 <rpj27n2xgd@privaterelay.appleid.com>
2026-07-18 12:06:39 +0000

    Phase 2B: add Installation meeting controls

--- assets/brimstone-calendar-masonic.js
+++ assets/brimstone-calendar-masonic.js
@@ -1,6 +1,6 @@
 /*
  * Brimstone Cottage Masonic Calendar UI
- * Phase 2A: event type, meeting pattern and meeting months.
+ * Phase 2B: event type, meeting pattern, months and Installation details.
  * Specialist recurrence saving is not wired yet.
  */
 (function () {
@@ -23,8 +23,60 @@
         otherRecurring: "Other recurring event",
         masonicOptions: "Masonic meeting options",
         meetingPattern: "Meeting pattern",
-        meetingMonths: "Meeting months"
+        meetingMonths: "Meeting months",
+        installationMonth: "Installation month",
+        installationDifferent: "Installation uses different details",
+        installationDetails: "Installation meeting details",
+        installationTitle: "Installation title",
+        installationStart: "Start time",
+        installationEnd: "End time",
+        installationNote: "Installation note"
     };
+
+    var months = [
+        { value: "1", shortName: "Jan", longName: "January" },
+        { value: "2", shortName: "Feb", longName: "February" },
+        { value: "3", shortName: "Mar", longName: "March" },
+        { value: "4", shortName: "Apr", longName: "April" },
+        { value: "5", shortName: "May", longName: "May" },
+        { value: "6", shortName: "Jun", longName: "June" },
+        { value: "7", shortName: "Jul", longName: "July" },
+        { value: "8", shortName: "Aug", longName: "August" },
+        { value: "9", shortName: "Sep", longName: "September" },
+        { value: "10", shortName: "Oct", longName: "October" },
+        { value: "11", shortName: "Nov", longName: "November" },
+        { value: "12", shortName: "Dec", longName: "December" }
+    ];
+
+    function meetingMonthsHtml() {
+        var html = "";
+        var i;
+
+        for (i = 0; i < months.length; i += 1) {
+            html +=
+                '<label><input type="checkbox" ' +
+                    'class="brimstone-masonic-month" ' +
+                    'value="' + months[i].value + '"> ' +
+                    months[i].shortName +
+                '</label>';
+        }
+
+        return html;
+    }
+
+    function installationMonthOptionsHtml() {
+        var html = '<option value="">None</option>';
+        var i;
+
+        for (i = 0; i < months.length; i += 1) {
+            html +=
+                '<option value="' + months[i].value + '">' +
+                    months[i].longName +
+                '</option>';
+        }
+
+        return html;
+    }
 
     function install() {
         var $ = window.jq || window.jQuery;
@@ -49,16 +101,20 @@
                     '<span class="label">' +
                         labels.eventType +
                     ':</span><br>' +
+
                     '<select aria-label="' +
                         labels.eventType +
                         '" style="margin-top:4px;min-width:220px;' +
                         'padding:5px 8px;">' +
+
                         '<option value="oneoff">' +
                             labels.oneOff +
                         '</option>' +
+
                         '<option value="masonic">' +
                             labels.masonic +
                         '</option>' +
+
                         '<option value="other">' +
                             labels.otherRecurring +
                         '</option>' +
@@ -84,6 +140,7 @@
                         '<select class="brimstone-masonic-order" ' +
                             'style="margin-top:4px;margin-right:6px;' +
                             'padding:5px 8px;">' +
+
                             '<option value="1">First</option>' +
                             '<option value="2">Second</option>' +
                             '<option value="3">Third</option>' +
@@ -93,6 +150,7 @@
 
                         '<select class="brimstone-masonic-weekday" ' +
                             'style="margin-top:4px;padding:5px 8px;">' +
+
                             '<option value="MO">Monday</option>' +
                             '<option value="TU">Tuesday</option>' +
                             '<option value="WE">Wednesday</option>' +
@@ -115,53 +173,95 @@
                                 'repeat(4,minmax(70px,1fr));' +
                             'gap:6px 10px;margin-top:6px;">' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="1"> Jan</label>' +
+                            meetingMonthsHtml() +
+                        '</div>' +
+                    '</div>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="2"> Feb</label>' +
+                    '<div class="brimstone-masonic-installation" ' +
+                        'style="margin-top:14px;padding-top:12px;' +
+                        'border-top:1px solid #666;">' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="3"> Mar</label>' +
+                        '<span class="label">' +
+                            labels.installationMonth +
+                        ':</span><br>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="4"> Apr</label>' +
+                        '<select class="brimstone-masonic-installation-month" ' +
+                            'style="margin-top:4px;min-width:180px;' +
+                            'padding:5px 8px;">' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="5"> May</label>' +
+                            installationMonthOptionsHtml() +
+                        '</select>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="6"> Jun</label>' +
+                        '<div style="margin-top:10px;">' +
+                            '<label>' +
+                                '<input type="checkbox" ' +
+                                    'class="brimstone-masonic-' +
+                                        'installation-different"> ' +
+                                labels.installationDifferent +
+                            '</label>' +
+                        '</div>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="7"> Jul</label>' +
+                        '<div class="brimstone-masonic-' +
+                            'installation-details" ' +
+                            'style="display:none;margin-top:12px;' +
+                            'padding:10px;border:1px solid #666;">' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="8"> Aug</label>' +
+                            '<strong>' +
+                                labels.installationDetails +
+                            '</strong>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="9"> Sep</label>' +
+                            '<div style="margin-top:10px;">' +
+                                '<label>' +
+                                    labels.installationTitle +
+                                    ':<br>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="10"> Oct</label>' +
+                                    '<input type="text" ' +
+                                        'class="brimstone-masonic-' +
+                                            'installation-title" ' +
+                                        'style="box-sizing:border-box;' +
+                                        'width:100%;margin-top:4px;' +
+                                        'padding:5px 8px;">' +
+                                '</label>' +
+                            '</div>' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="11"> Nov</label>' +
+                            '<div style="display:flex;flex-wrap:wrap;' +
+                                'gap:12px;margin-top:10px;">' +
 
-                            '<label><input type="checkbox" ' +
-                                'class="brimstone-masonic-month" ' +
-                                'value="12"> Dec</label>' +
+                                '<label>' +
+                                    labels.installationStart +
+                                    ':<br>' +
+
+                                    '<input type="time" ' +
+                                        'class="brimstone-masonic-' +
+                                            'installation-start" ' +
+                                        'style="margin-top:4px;' +
+                                        'padding:5px 8px;">' +
+                                '</label>' +
+
+                                '<label>' +
+                                    labels.installationEnd +
+                                    ':<br>' +
+
+                                    '<input type="time" ' +
+                                        'class="brimstone-masonic-' +
+                                            'installation-end" ' +
+                                        'style="margin-top:4px;' +
+                                        'padding:5px 8px;">' +
+                                '</label>' +
+                            '</div>' +
+
+                            '<div style="margin-top:10px;">' +
+                                '<label>' +
+                                    labels.installationNote +
+                                    ':<br>' +
+
+                                    '<textarea class="brimstone-masonic-' +
+                                        'installation-note" rows="3" ' +
+                                        'style="box-sizing:border-box;' +
+                                        'width:100%;margin-top:4px;' +
+                                        'padding:5px 8px;"></textarea>' +
+                                '</label>' +
+                            '</div>' +
                         '</div>' +
                     '</div>' +
                 '</div>'
@@ -170,7 +270,38 @@
             repeat.before(type);
             type.after(masonic);
 
-            function update() {
+            var installationMonth = masonic.find(
+                ".brimstone-masonic-installation-month"
+            );
+
+            var installationDifferent = masonic.find(
+                ".brimstone-masonic-installation-different"
+            );
+
+            var installationDetails = masonic.find(
+                ".brimstone-masonic-installation-details"
+            );
+
+            function updateInstallation() {
+                var hasInstallationMonth =
+                    installationMonth.val() !== "";
+
+                installationDifferent.prop(
+                    "disabled",
+                    !hasInstallationMonth
+                );
+
+                if (!hasInstallationMonth) {
+                    installationDifferent.prop("checked", false);
+                }
+
+                installationDetails.toggle(
+                    hasInstallationMonth &&
+                    installationDifferent.prop("checked")
+                );
+            }
+
+            function updateEventType() {
                 var value = type.find("select").val();
 
                 masonic.toggle(value === "masonic");
@@ -186,8 +317,23 @@
                 }
             }
 
-            type.find("select").on("change", update);
-            update();
+            installationMonth.on(
+                "change",
+                updateInstallation
+            );
+
+            installationDifferent.on(
+                "change",
+                updateInstallation
+            );
+
+            type.find("select").on(
+                "change",
+                updateEventType
+            );
+
+            updateInstallation();
+            updateEventType();
         });
     }
 
